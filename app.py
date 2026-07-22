831ba91fbfa70e74b70f00ac8df82c383b517bfb
candraakirana31-prog <candraakirana31@gmail.com>
2026-07-22 14:24:20 +0700

    perubahan app

--- app.py
+++ app.py
@@ -18,13 +18,12 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
 
-    with app.app_context():
-        db.create_all()
-
     return app
 
 
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
